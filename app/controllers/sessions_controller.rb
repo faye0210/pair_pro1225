1936db83a110cd2a3a2cf427ebad62c70b0115a0
Faye.H <yang5chen12hui@gmail.com>
2020-12-25 17:40:40 +0900

    ログアウト機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,5 +10,10 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
+    def destroy
+      session.delete(:user_id)
+      flash[:notice] = 'ログアウトしました'
+      redirect_to new_session_path
+    end
   end
 end
